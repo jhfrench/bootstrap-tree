25b7c00cf248c3468a3edee6d44ea6b70e2f0d5a
piettes <mail@stephanepiette.com>
2014-05-26 19:18:45 +0200

    Performance improvement
    If there are a lot of 'li', it's getting really slow to open/close a
    tree node, because all 'li's are changed. Just changing the visibility
    of the whole 'ul' instead greatly improves performances

--- js/bootstrap-tree.js
+++ js/bootstrap-tree.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$('.tree > ul').attr('role', 'tree').find('ul').attr('role', 'group');
 	$('.tree').find('li:has(ul)').addClass('parent_li').attr('role', 'treeitem').find(' > span').attr('title', 'Collapse this branch').on('click', function (e) {
-        var children = $(this).parent('li.parent_li').find(' > ul > li');
+        var children = $(this).parent('li.parent_li').find(' > ul');
         if (children.is(':visible')) {
     		children.hide('fast');
     		$(this).attr('title', 'Expand this branch').find(' > i').addClass('icon-plus-sign').removeClass('icon-minus-sign');
